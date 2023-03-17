e7c533d42dadd847ecf01ba14909fdb5158325e5
itsjosehernandez <joseher2702@gmail.com>
2023-03-17 00:37:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generate() {
+  let who = ["The dog", "my granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let towhat = ["my homework", "my car", "my shoes", "my bike"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch"
+  ];
+  let rand_who = Math.floor(Math.random() * who.length);
+  let rand_what = Math.floor(Math.random() * what.length);
+  let rand_towhat = Math.floor(Math.random() * towhat.length);
+  let rand_when = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("result").innerHTML =
+    "<h2>" +
+    who[rand_who] +
+    " " +
+    what[rand_what] +
+    " " +
+    towhat[rand_towhat] +
+    " " +
+    when[rand_when] +
+    "</h2>";
+}
---
+++ src/prueba.js
@@ -0,0 +1,14 @@
+
+function generate(){
+    let who = ["The dog", "my granma", "His turtle", "My bird"];
+     let what = ["eat", "pissed", "crushed", "broked"];
+     let towhat = ["my homework", "my car", "my shoes", "my bike"];
+     let when = ["before the class", "right in time","when I finished","during my lunch"];
+    let rand_who = Math.floor(Math.random()*who.length);
+    let rand_what = Math.floor(Math.random()*what.length);
+    let rand_towhat = Math.floor(Math.random()*towhat.length);
+    let rand_when = Math.floor(Math.random()*when.length);
+        
+	document.getElementById('result').innerHTML = "<h2>"+who[rand_who]+" "+what[rand_what]+" "+towhat[rand_towhat]+" "+when[rand_when]+"</h2>";
+	
+}
